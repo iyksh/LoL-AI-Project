8541e40d19729a0101a1f143e7c16595be33a03d
devYaksha <gssantoz2012@gmail.com>
2023-10-21 13:42:54 -0300

    run changes

--- run.py
+++ run.py
@@ -1,10 +1,11 @@
 import os
 
-os.system("pip install -r src/configs/requirements.txt")
-
 if os.name == "nt": #nt: windows os
+        os.system("pip install -r src/configs/requirements.txt")
         os.system("python3 src\main.py")
 else:
-    os.system("python3 src/main.py")
+        os.system("pip install -r src/configs/requirements.txt")
+        os.system("python3 src/main.py")     
+  
 
 
