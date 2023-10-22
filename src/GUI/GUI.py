053e37ee9f3e1d6faf308102ccd9206a333e44a6
devYaksha <gssantoz2012@gmail.com>
2023-10-22 10:10:19 -0300

    Interface changes

--- src/GUI/GUI.py
+++ src/GUI/GUI.py
@@ -1,9 +1,9 @@
 import tkinter as tk
 from tkinter import ttk
 import subprocess
-from Lol import header as h
+
 campeoes_lol = [
-   "aatrox", "ahri", "akali", "akshan", "alistar", "amumu", "anivia", "annie", "aphelios", "ashe",
+    "aatrox", "ahri", "akali", "akshan", "alistar", "amumu", "anivia", "annie", "aphelios", "ashe",
     "aurelion sol", "azir", "bard", "bel'veth", "blitzcrank", "brand", "braum", "briar", "caitlyn",
     "camille", "cassiopeia", "cho'gath", "corki", "darius", "diana", "dr. mundo", "draven", "ekko",
     "elise", "evelynn", "ezreal", "fiddlesticks", "fiora", "fizz", "galio", "gangplank", "garen", "gnar",
@@ -23,57 +23,82 @@ campeoes_lol = [
 ]
 
 
+def autocomplete(event):
+    search_text = event.widget.get().lower()
+
+    if len(search_text) >= 2:
+        matches = [champion for champion in campeoes_lol if search_text in champion.lower()]
+        event.widget['values'] = matches
+        event.widget.event_generate('<Down>')
+
+
+def create_autocomplete_combobox(parent, label_text, default_value):
+    label = tk.Label(parent, text=label_text)
+    combobox = ttk.Combobox(parent)
+    combobox['values'] = campeoes_lol
+    combobox.set(default_value)
+    combobox.bind('<KeyRelease>', autocomplete)
+
+    label.pack()
+    combobox.pack()
+
+    return combobox
+
+
 def start_game():
     input_window = tk.Toplevel(root)
     input_window.title("Input Window")
 
-    champ_label = tk.Label(input_window, text="Champion to play:")
-    champ_combobox = ttk.Combobox(input_window, values=campeoes_lol)  
-    champ_combobox.set("aatrox")  # Defina um valor padrão
-    ban_label = tk.Label(input_window, text="Champion to ban:")
-    ban_combobox = ttk.Combobox(input_window, values=campeoes_lol)  
-    ban_combobox.set("aatrox")  # Defina um valor padrão
+    champ_combobox = create_autocomplete_combobox(input_window, "Champion to play:", "")
+    ban_combobox = create_autocomplete_combobox(input_window, "Champion to ban:", "")
+
     start_button = tk.Button(input_window, text="Start Game", command=lambda: start_game_with_champs(champ_combobox.get(), ban_combobox.get()))
 
-    champ_label.pack()
-    champ_combobox.pack()
-    ban_label.pack()
-    ban_combobox.pack()
     start_button.pack()
+
 
 def start_game_with_champs(champ_to_play, champ_to_ban):
     print(champ_to_play, champ_to_ban)
     subprocess.call([h.AutoAccept_Match(champ_to_play, champ_to_ban)])
 
+
 def search_runes():
     input_window = tk.Toplevel(root)
     input_window.title("Search Runes")
 
-    champ_label = tk.Label(input_window, text="Champion to search:")
-    champ_combobox = ttk.Combobox(input_window, values=campeoes_lol) 
-    champ_combobox.set("aatrox")  # Defina um valor padrão
+    champ_combobox = create_autocomplete_combobox(input_window, "Champion to search:", "")
     lane_label = tk.Label(input_window, text="Lane")
-    lane_combobox = ttk.Combobox(input_window, values=["top", "jungle", "middle", "bottom", "support"])  
-    lane_combobox.set("top")  # Defina um valor padrão
+    lane_combobox = ttk.Combobox(input_window, values=["top", "jungle", "middle", "bottom", "support"])
+    lane_combobox.set("")
+
     search_button = tk.Button(input_window, text="Search", command=lambda: h.open_runeSite(champ_combobox.get(), lane_combobox.get()))
 
-    champ_label.pack()
-    champ_combobox.pack()
     lane_label.pack()
     lane_combobox.pack()
     search_button.pack()
 
+def startGui():
+    root.geometry("300x300")
+    root.mainloop()
+    
+def teamFightTatics():
+    window = tk.Tk()
+    window.title("Under Construction")
+    label = tk.Label(window, text="In Construction", font=("Arial", 20))
+    label.pack(padx=20, pady=20)
+
+    # Run the tkinter main loop
+    window.mainloop()
+
+
 root = tk.Tk()
 root.title("AutoClicker")
+buttonAAM = tk.Button(root, text="Auto-Accept Match", command=start_game,  width=50, height=2)
+buttonTFT = tk.Button(root, text="Teamfight Tactics", command=teamFightTatics,  width=40, height=2)
+buttonSR = tk.Button(root, text="Search Runes", command=search_runes,  width=30, height=2)
 
-button1 = tk.Button(root, text="Auto-Accept Match", command=start_game)
-button5 = tk.Button(root, text="Search Runes", command=search_runes)
-
-button1.pack()
-button5.pack()
-
-def startGui():
-    root.geometry("300x200")
-    root.mainloop()
+buttonAAM.pack()
+buttonTFT.pack()
+buttonSR.pack()
 
 startGui()
